a51015b114ef1d3f90a9e2ab4c5a1031ce17fd4a
jambrocio-2022262 <jambrocio-2022262@kinal.edu.gt>
2024-02-02 09:52:27 -0600

    Le agregamos las otras funciones al JS

--- assets/logica.js
+++ assets/logica.js
@@ -7,3 +7,18 @@ function DetalleContacto2(){
     var detalle = document.getElementById('Detalle2');
     detalle.style.display = 'flex';
 }
+
+function DetalleContacto3(){
+    var detalle = document.getElementById('Detalle2');
+    detalle.style.display = 'flex';
+}
+
+function DetalleContacto4(){
+    var detalle = document.getElementById('Detalle2');
+    detalle.style.display = 'flex';
+}
+
+function DetalleContacto5(){
+    var detalle = document.getElementById('Detalle2');
+    detalle.style.display = 'flex';
+}
