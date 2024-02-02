bc59214b28cb866ad0a4a6996e36e46e45ed9f85
jambrocio-2022262 <jambrocio-2022262@kinal.edu.gt>
2024-02-02 09:34:32 -0600

    Le re diseñamos todo el codigo, le agregamos las estrellas que iban a hacer de contactos favortios y hicimos que al precionar detalle contacto dependiendo del contacto mandara a llamar la informacion que solicitamos y hicimos para eso modificaciones en el JS

--- assets/logica.js
+++ assets/logica.js
@@ -1,9 +1,9 @@
-function DetalleContacto(){
+function DetalleContacto1(){
     var detalle = document.getElementById('Detalle1');
     detalle.style.display = 'flex';
 }
 
-function DetalleContacto(){
+function DetalleContacto2(){
     var detalle = document.getElementById('Detalle2');
     detalle.style.display = 'flex';
 }
