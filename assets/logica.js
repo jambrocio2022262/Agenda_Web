19caf3d73a82165e293dd2b0d5e2ca05588efe1e
jambrocio-2022262 <jambrocio-2022262@kinal.edu.gt>
2024-02-02 08:45:06 -0600

    Se agregaron cambios para mejorar programa

--- assets/logica.js
+++ assets/logica.js
@@ -2,3 +2,8 @@ function DetalleContacto(){
     var detalle = document.getElementById('Detalle1');
     detalle.style.display = 'flex';
 }
+
+function DetalleContacto(){
+    var detalle = document.getElementById('Detalle2');
+    detalle.style.display = 'flex';
+}
