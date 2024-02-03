08aa1e40417d17167f0ed2eadd17511bbc08c4a4
jambrocio-2022262 <jambrocio-2022262@kinal.edu.gt>
2024-02-02 20:28:08 -0600

    Terminamos de agregar el detalle a cada contacto y le pusimos el css

--- assets/logica.js
+++ assets/logica.js
@@ -9,16 +9,16 @@ function DetalleContacto2(){
 }
 
 function DetalleContacto3(){
-    var detalle = document.getElementById('Detalle2');
+    var detalle = document.getElementById('Detalle3');
     detalle.style.display = 'flex';
 }
 
 function DetalleContacto4(){
-    var detalle = document.getElementById('Detalle2');
+    var detalle = document.getElementById('Detalle4');
     detalle.style.display = 'flex';
 }
 
 function DetalleContacto5(){
-    var detalle = document.getElementById('Detalle2');
+    var detalle = document.getElementById('Detalle5');
     detalle.style.display = 'flex';
 }
